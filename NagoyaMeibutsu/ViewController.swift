f2e383fc30d38b129e61f9b1086cae17b7d80255
Kenichiro Sato <satok0702@gmail.com>
2015-04-15 22:07:50 +0900

    Initial Commit

---
+++ NagoyaMeibutsu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NagoyaMeibutsu
+//
+//  Created by 佐藤健一朗 on 2015/04/15.
+//  Copyright (c) 2015年 Kenichiro Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
