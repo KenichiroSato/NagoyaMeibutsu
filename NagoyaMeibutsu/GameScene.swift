f3755900cdcb73209e526068a258c02429c3122e
Kenichiro Sato <satok0702@gmail.com>
2015-04-15 23:17:02 +0900

    added background and Danbury

--- NagoyaMeibutsu/GameScene.swift
+++ NagoyaMeibutsu/GameScene.swift
@@ -11,4 +11,25 @@ import SpriteKit
 
 class GameScene : SKScene {
     
+    override func didMoveToView(view: SKView) {
+        self.setBackground()
+        self.fallNagoyaSpecialty()
+    }
+    
+    func setBackground() {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: self.size.width * 0.5, y: self.size.height * 0.5)
+        background.size = self.size
+        
+        self.addChild(background)
+    }
+    
+    func fallNagoyaSpecialty() {
+        let texture = SKTexture(imageNamed: "0")
+        let sprite = SKSpriteNode(texture: texture)
+        sprite.position = CGPointMake(self.size.width * 0.5, self.size.height * 0.5)
+        sprite.size = CGSize(width: sprite.size.width * 0.5, height: sprite.size.height * 0.5)
+        
+        self.addChild(sprite)
+    }
 }
